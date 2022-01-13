85ea83c40db17b628d58f04932ebc05905b54878
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2022-01-12 23:23:54 -0600

    use gust if possible

--- app/views.py
+++ app/views.py
@@ -6,7 +6,6 @@ import requests
 
 cards = { 0: u"↓", 45: u"↙", 90: u"←", 135: u"↖", 180: u"↑", 225: u"↗", 270: u"→", 315: u"↘", 360: u"↓" }
 
-
 @app.route('/weather/<location>', strict_slashes=False, methods=['GET'])
 @cache.memoize(300)
 def index(location):
@@ -25,16 +24,17 @@ def index(location):
         weather = requests.get(owm_url, params=params).json()
     except Exception as e:
         print(e)
-        return "OpenWeather API error"
+        return "OpenWeatherMap API error"
 
     try:
         direction = cards.get(float(weather['current']['wind_deg']),
             cards[min(cards.keys(), key=lambda k: abs(k - float(weather['current']['wind_deg'])))])
 
         weather['current']['weather'][0]['description'] = weather['current']['weather'][0]['description'].replace('intensity ', '')
+        weather['current']['wind_gust'] = weather['current'].get('wind_gust', weather['current']['wind_speed'])
 
         return u"{current[temp]:.0f}\u00b0F({current[feels_like]:.0f}\u00b0F) " \
-            u"{current[weather][0][description]} {direction}{current[wind_speed]:.0f}mph " \
+            u"{current[weather][0][description]} {direction}{current[wind_gust]:.0f}mph " \
             u"{current[humidity]:.0f}%".format(direction=direction, **weather)
     except Exception as e:
         print(e)
